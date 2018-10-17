8795de0b9e16269f3a6f2dc4a74513ce8540bb73
fabioevandro <fabioevandro@gmail.com>
2018-10-17 05:13:45 -0300

    mudei o primeiro echo e adicionei o segundo echo

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <div style="font-size:52px;padding:50px;border:1px solid #000;text-align:center;margin:50px;background-color: #ddd">
 <?php
 
-echo "Olá pra todo mundo: Editado";
+echo "Olá pra todo mundo: Editado --> Máquina";
+
+echo "Olá Olá Olá tem alguem ai?";
 
 ?>
 </div>
