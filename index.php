188ba6ce1767f43405fc45ed5b7398d3dc60edc3
fabioevandro <44053986+fabioevandro@users.noreply.github.com>
2018-10-17 05:02:26 -0300

    Primeira alteração

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <div style="font-size:52px;padding:50px;border:1px solid #000;text-align:center;margin:50px;background-color: #ddd">
 <?php
 
-echo "Olá pra todo mundo";
+echo "Olá pra todo mundo: Editado";
 
 ?>
 </div>
